5853406cb67810879220caf21a3fa3bdf71dcdb7
Arnulfo Solis <arnulfojr94@gmail.com>
2017-11-19 22:01:42 +0100

    final base server core

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -1,16 +1,16 @@
 
 import os
 
-from db import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
+from db import DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_ECHO, DB_URI
 
 
 PORT = os.getenv('PORT', 5000)
 
 HOSTNAME = os.getenv('HOST', '0.0.0.0')
 
-IS_DEV = os.getenv('DEV', 'True') == 'True'
+IS_DEV = os.getenv('IS_DEV', 'True') == 'True'
 
-USE_RELOADER = IS_DEV and os.getenv('USE_RELOADER', 'True') == 'True'
+USE_RELOADER = IS_DEV and (os.getenv('USE_RELOADER', 'True') == 'True')
 
-USE_DEBUGGER = IS_DEV and os.getenv('USE_DEBUGGER', 'True') == 'True'
+USE_DEBUGGER = IS_DEV and (os.getenv('USE_DEBUGGER', 'True') == 'True')
 
